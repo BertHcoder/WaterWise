57bc9902be3869676b0ff7a9b6452939814e45e8
DIRTYmasterchief <dirtymasterchief@gmail.com>
2025-04-28 21:49:00 +0200

    Add Data project and Entity Framework Core support
    - Updated `WaterWiseAPI.csproj` to include `Microsoft.EntityFrameworkCore.Design`.
    - Modified `WaterWiseBackEnd.sln` to add a reference to the new `Data` project.
    - Created `Data.csproj` targeting `.NET 8.0` with `Microsoft.EntityFrameworkCore`.
    - Added `DatabaseContext`, `Aquarium`, `WaterParameter`, and `WaterParameterHealthScore` classes.
    - Generated new NuGet management files and updated assembly references.
    - Introduced global usings and an autogenerated `.editorconfig` for coding styles.

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Data.entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Aquarium> Aquariums{ get; set; }
+    }
+}
---
+++ Data/entities/WaterParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.entities
+{
+    public class WaterParameter
+    {
+        public double PH { get; set; }
+        public double Ammonia { get; set; }
+        public double Nitrite { get; set; }
+        public double Nitrate { get; set; }
+        public double Hardness { get; set; }
+        public double Temperature { get; set; }
+        public double Phosphate { get; set; }
+    }
+}
